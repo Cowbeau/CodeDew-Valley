d66a05c840d26e817cc2b44f319b720c7a26bbdf
beaum <beaumccoy95@gmail.com>
2023-08-12 19:47:08 -0500

    feat: :sparkles: Create player/enemy interactions
    Create player/enemy interactions like attacks, knockback, and health bar damage

--- Legend Of DarkZouls/code/player.py
+++ Legend Of DarkZouls/code/player.py
@@ -40,7 +40,12 @@ class Player(Entity):
 
 
 
-        def import_player_assets(self):
+            # damage timer
+        self.vulnerable = True
+        self.hurt_time = None
+        self.invulnerability_duration = 500
+
+    def import_player_assets(self):
             character_path = '/home/beaum/Documents/Coding Projects/Python Projects/Legend Of DarkZouls/graphics/player/'
             self.animations = {'up':[],'down':[],'left':[],'right':[],
             'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
@@ -120,6 +125,32 @@ class Player(Entity):
             else:
                 if 'attack' in self.status:
                     self.status = self.status.replace('_attack','')
+    def move(self,speed):
+            if self.direction.magnitude() != 0:
+                self.direction.normalize()
+
+            self.hitbox.x += self.direction.x * speed
+            self.collision('horizontal')
+            self.hitbox.y += self.direction.y * speed
+            self.collision('vertical')
+            self.rect.center = self.hitbox.center
+
+    def collision(self,direction):
+                if direction == 'horizontal':
+                    for sprite in self.obstacle_sprites:
+                        if sprite.hitbox.colliderect(self.hitbox):
+                            if self.direction.x > 0: # moving right
+                                self.hitbox.right = sprite.hitbox.left
+                            if self.direction.x < 0: # moving left
+                                self.hitbox.left = sprite.hitbox.right
+
+                if direction == 'vertical':
+                    for sprite in self.obstacle_sprites:
+                        if sprite.hitbox.colliderect(self.hitbox):
+                            if self.direction.y > 0: # moving down
+                                self.hitbox.bottom = sprite.hitbox.top
+                            if self.direction.y < 0: # moving up
+                                self.hitbox.top = sprite.hitbox.bottom
 
         def move(self,speed):
             if self.direction.magnitude() != 0:
@@ -132,7 +163,7 @@ class Player(Entity):
             self.rect.center = self.hitbox.center
             
 
-        def collision(self,direction):
+    def collision(self,direction):
                 if direction == 'horizontal':
                     for sprite in self.obstacle_sprites:
                         if sprite.hitbox.colliderect(self.hitbox):
@@ -153,16 +184,25 @@ class Player(Entity):
             current_time = pygame.time.get_ticks()
 
             if self.attacking:
-                if current_time - self.attack_time >= self.attack_cooldown:
+                if current_time - self.attack_time >= self.attack_cooldown + weapon_data[self.weapon]['cooldown']:
                     self.attacking = False 
                     self.destroy_attack()
 
             if  not self.can_switch_weapon:
                 if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
                     self.can_switch_weapon = True
+            
+            if not self.can_switch_magic:
+                if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+                    self.can_switch_magic = True
 
-        def animate(self):
-            animation = self.animations[self.satus]
+
+            if  not self.can_switch_magic:
+                if current_time - self.magic_switch_time >= self.switch_duration_cooldown:
+                    self.can_switch_magic = True
+
+    def animate(self):
+            animation = self.animations[self.status]
 
             # loop over frame index
             self.frame_index += self.animation_speed
@@ -173,6 +213,18 @@ class Player(Entity):
             self.image = animation[int(self.frame_index)]
             self.rect = self.image.get_rect(center = self.hitbox.center)
 
+            # flicker
+            if not self.vulnerable:
+                alpha = self.wave_value()
+                self.image.set_alpha(alpha)
+            else:
+                self.image.set_alpha(255)
+
+    def get_full_weapon_damage(self):
+       base_damage = self.stats['attack']
+       weapon_damage = weapon_data[self.weapon]['damage']
+       return base_damage + weapon_damage
+    
     def update(self):
             self.input
             self.cooldowns
