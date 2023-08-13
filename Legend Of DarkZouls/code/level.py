93caaccfe6dc3e22c73230c336c27a4c454b389b
beaum <beaumccoy95@gmail.com>
2023-08-12 22:30:13 -0500

    feat: :sparkles: Create Particle Effects
    Created particle effects for attack animations, monster deaths, and grass destruction

--- Legend Of DarkZouls/code/level.py
+++ Legend Of DarkZouls/code/level.py
@@ -3,10 +3,11 @@ from settings import *
 from tile import Tile
 from player import Player 
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
+from particles import AnimationPlayer
 
 class Level:
     def __init__(self):
@@ -28,6 +29,9 @@ class Level:
 
         # user interface
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer()
 
     def create_map(self):
         # layouts = {
@@ -75,7 +79,8 @@ class Level:
                                 elif col == '391': monster_name = 'spirit'
                                 elif col == '392': monster_name = 'raccoon'
                                 else: monster_name = 'squid'
-                                Enemy(monster_name,(x,y),[self.visible_sprites],self.obstacle_sprites)
+                                Enemy(monster_name,(x,y),[self.visible_sprites],self.obstacle_sprites,
+                                self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player,[self.visible_sprites,self.attack_sprites])
@@ -97,6 +102,10 @@ class Level:
                 if collision_sprites:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
+                            pos = target_sprite.rect.center
+                            offset = pygame.math.Vector2(0,75)
+                            for leaf in range(randint(3,6)):
+                                self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player,attack_sprite.sprite_type)
@@ -106,7 +115,11 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
-            # spawn particles
+            self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
+
+    def trigger_death_particles(self,pos,particle_type):
+
+        self.animation_player.create_particles(particle_type,pos,self.visible_sprites)
 
     def run(self):
         #update and draw the games
